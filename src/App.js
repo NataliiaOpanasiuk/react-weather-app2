b9db0fd90fbd0013635fde30e2bf99a25321c3c1
NataliiaOpanasiuk <nataopanasiuk@gmail.com>
2024-07-01 20:31:03 -0700

    Added noreferrer attribute to links

--- src/App.js
+++ src/App.js
@@ -9,11 +9,16 @@ export default function App() {
         <a
           href="https://github.com/NataliiaOpanasiuk/react-weather-app2"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced on GitHub
         </a>{" "}
         and{" "}
-        <a href="https://react-weather-app-on.netlify.app/" target="_blank">
+        <a
+          href="https://react-weather-app-on.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
           hosted on Netlify
         </a>
         .
