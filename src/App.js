1cfb15c21e48457d2f40d953ae798fb43eb96e3a
NataliiaOpanasiuk <nataopanasiuk@gmail.com>
2024-07-01 19:48:44 -0700

    Added a link to Git repository

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <p>
+        This project was created by Nataliia Opanasiuk and is {""}
+        <a
+          href="https://github.com/NataliiaOpanasiuk/react-weather-app2"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+        .
+      </p>
+    </div>
+  );
 }
