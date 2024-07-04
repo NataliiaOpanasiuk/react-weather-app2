61bc47b0337fce6dc3abe2c55cae86d3e043e094
NataliiaOpanasiuk <nataopanasiuk@gmail.com>
2024-07-03 21:49:16 -0700

    Failed try to add Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,26 +3,31 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <p>
-        This project was created by Nataliia Opanasiuk and is {""}
-        <a
-          href="https://github.com/NataliiaOpanasiuk/react-weather-app2"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://react-weather-app-on.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          hosted on Netlify
-        </a>
-        .
-      </p>
+      <div className="container">
+        <h1>Weather App</h1>
+        <button type="button" class="btn btn-primary">
+          Primary
+        </button>
+        <p>
+          This project was created by Nataliia Opanasiuk and is {""}
+          <a
+            href="https://github.com/NataliiaOpanasiuk/react-weather-app2"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://react-weather-app-on.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+          .
+        </p>
+      </div>
     </div>
   );
 }
